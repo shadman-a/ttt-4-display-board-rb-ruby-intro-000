4beeb1fe1fdc91a71682281c59baf31ac82ce3b0
Shadman Ahmed <shadman@outlook.com>
2020-03-23 23:13:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 def display_board(spot)
-  puts "#{spot[0]}   | #{spot[1]}  | #{spot[2]}  "
+  puts "#{spot[0]}| #{spot[1]}| #{spot[2]}"
   puts "-----------"
-  puts " #{spot[3]}  | #{spot[4]}  |  #{spot[5]} "
+  puts "#{spot[3]}| #{spot[4]}| #{spot[5]}"
   puts "-----------"
-  puts " #{spot[6]}  | #{spot[7]}  |  #{spot[8]} "
+  puts "#{spot[6]}| #{spot[7]}| #{spot[8]}"
 end
