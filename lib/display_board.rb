fca3c14519bbd0866c4f04e584a4940eaa695c80
Shadman Ahmed <shadman@outlook.com>
2020-03-23 23:32:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
 board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-row = " | "
-line = " ----------- "
 
 def display_board(spot)
-  puts "#{spot[0]} #{row} #{spot[1]} #{row} #{spot[2]}"
-  puts "#{line}"
-  puts "#{spot[3]} #{row} #{spot[4]} #{row} #{spot[5]}"
-  puts "#{line}"
-  puts "#{spot[6]} #{row} #{spot[7]} #{row} #{spot[8]}"
+  puts "#{spot[0]} | #{spot[1]} | #{spot[2]}"
+  puts "-----------"
+  puts "#{spot[3]} | #{spot[4]} | #{spot[5]}"
+  puts "-----------"
+  puts "#{spot[6]} | #{spot[7]} | #{spot[8]}"
 end
