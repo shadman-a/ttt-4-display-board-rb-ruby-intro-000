9bcd51ca4a74974b790afcc5a91b712ae511e84b
Shadman Ahmed <shadman@outlook.com>
2020-03-23 23:08:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+def display_board(spot)
+  puts "#{spot[0]}   | #{spot[1]}  | #{spot[2]}  "
+  puts "-----------"
+  puts " #{spot[3]}  | #{spot[4]}  |  #{spot[5]} "
+  puts "-----------"
+  puts " #{spot[6]}  | #{spot[7]}  |  #{spot[8]} "
+end
